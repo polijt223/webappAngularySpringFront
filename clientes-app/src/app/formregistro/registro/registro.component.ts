750f35edc8200a26cc320711317657bc405ac123
polijt223 <pablotulian10@gmail.com>
2019-03-15 14:09:57 -0300

    Se agrega componenete y servicio registro

---
+++ clientes-app/src/app/formregistro/registro/registro.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from '../../usuarios/usuarios.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
+import { Usuario } from 'src/app/usuarios/usuario';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  private errores: string[];
+  private usuario: Usuario = new Usuario();
+
+
+  constructor(private usuarioService:UsuariosService,
+              private router:Router,
+              private activateRoute: ActivatedRoute
+              ) { }
+              
+
+  ngOnInit() {
+  }
+
+  create(): void{
+    this.usuario.roles = ['ROLE_USER'],['ROLE_ADMIN'];
+    console.log(this.usuario);
+    
+    this.usuarioService.create(this.usuario)
+    .subscribe( usuario => {
+      this.router.navigate(['/login']);
+      Swal.fire({title: 'Cliente Guardado',text: `El Usuario ${usuario.nombre} se registro correctamente`,type: 'success',confirmButtonText: 'Aceptar' });
+    },
+    err => {
+      this.errores = err.error.errors as string[];
+      console.error('Código de error desde el backend: ' + err.status);
+      console.error(err.error.errors);
+    }
+    );
+    
+  }
+
+}
---
+++ clientes-app/src/app/usuarios/usuarios.service.ts
@@ -0,0 +1,72 @@
+import { Injectable } from '@angular/core';
+import { of,Observable,throwError } from 'rxjs';
+//import { of } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpRequest, HttpEvent } from '@angular/common/http';
+import { map,catchError,tap } from 'rxjs/operators';
+import Swal from 'sweetalert2';
+import {Router} from '@angular/router';
+import { AuthService } from './auth.service';
+import { Usuario } from './usuario';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UsuariosService {
+
+  private urlEndPoint: string = 'http://localhost:7075/user/usuarios';
+  private httpHeaders = new HttpHeaders({'Content-Type':'application/json'});
+
+  constructor(private http: HttpClient,
+              private router:Router,
+              private authService: AuthService
+  ) { }
+
+  create(usuario: Usuario) : Observable<Usuario>{
+    return this.http.post(this.urlEndPoint,usuario,{headers: this.agregarAuthorizationHeader()}).pipe(    // pasamos el metodo agregarAuthorizationHeader para que la cabesera pasada tenga la autorizacion tambien
+      map((response:any) => response.usuario as Usuario),
+      catchError(e => {
+
+        if (this.isNoAutorizado(e)) {
+          return throwError(e);
+        }
+
+        if (e.status == 400) {
+          return throwError(e);
+        }
+
+        console.error(e.error.mensaje);
+        Swal.fire({ title: e.error.mensaje, text: e.error.error, type: 'error', confirmButtonText: 'Aceptar'});
+        return throwError(e);
+      })
+    );
+  }
+
+  private isNoAutorizado(e):boolean{
+    if (e.status==401) {  //Error 401 quiere decir que no pueder acceder al recurso no tienes credenciales no existe no disponible
+
+      if (this.authService.isAuthenticated()) {   //Preguntamos si el token existe, si el token existe devolvera true, Entonce el servido me dice que no puedo acceder y el front me da el okey  del token
+        this.authService.logout();    //Esto se produce porque la session del backend expiro y no se cerro la sesion del frontend , para solucionarlo cerramos la session para que se vuelva a loguear
+      }
+      this.router.navigate(['/login']);   // mandamos al usuario al log por no estar logueado
+      return true;
+    }
+    if (e.status==403) {
+      Swal.fire({ title: 'Acceso Denegado', text: `Hola ${this.authService.usuario.username} no tienes acceso a este recurso`, type: 'warning', confirmButtonText: 'Aceptar'});
+      this.router.navigate(['/clientes']);
+      return true;
+    }
+  }
+
+  private agregarAuthorizationHeader(){
+    let token = this.authService.token;   //token es un metodo get 
+    if (token != null) {            //comprueba que haya un token en el sessionStorege en caso de que si haya un token , agregamos la autorización 
+      return this.httpHeaders.append('Authorization','Bearer ' + token);
+    }
+    return this.httpHeaders;   // sino lo retornamos como estaba , sin autorizacion por lo que tendra que loguearse
+
+  }
+
+
+}
